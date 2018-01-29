88662c123aa0f7692c677f25b4e02d79e60f1079
Dean Hamann <hamann.dean@gmail.com>
2018-01-28 16:31:10 -0800

    Movie show page styling

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import userService from '../../utils/userService';
 import './LoginForm.css';
-import { Button } from 'react-materialize';
+import { Button } from 'react-bootstrap';
 
 class LoginForm extends Component {
   constructor(props) {
--- src/components/MovieShow/MovieShow.jsx
+++ src/components/MovieShow/MovieShow.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MovieShow.css';
 import ReactLoading from 'react-loading';
+import {Grid, Col} from 'react-bootstrap';
 
 
 const MovieShow = (props) => {
@@ -9,16 +10,24 @@ const MovieShow = (props) => {
     <div className="movieBox">
       {
         props.movie ?
-          <div>
-            <div>
-              <h1>{props.movie.title}</h1>
-              <span>"{props.movie.tagline}"</span>
-            </div>
-            <div>
-              <img id="postertest" class="poster" src={`https://image.tmdb.org/t/p/w342/${props.movie.poster_path}`} alt="Movie Poster" />
-            </div>
-          </div>
-
+        <Grid> 
+        <Col xs={12}> 
+         <Col xs={12} md={8} mdPush={5} lg={7} lgPush={5}> 
+             <h1>{props.movie.title}</h1>
+             <span>"{props.movie.tagline}"</span>
+             <p>{props.movie.overview}</p>
+             <div className="row nopadding release-details">
+               <Col xs={6}> Original Release: <span className="meta-data">{props.movie.release_date}</span></Col>
+               <Col xs={6}> Running Time: <span className="meta-data">{props.movie.runtime} mins</span> </Col>
+               <Col xs={6}> Box Office: <span className="meta-data">{props.movie.revenue}</span></Col>
+               <Col xs={6}> Vote Average: <span className="meta-data">{props.movie.vote}</span></Col>
+             </div>
+         </Col>
+         <Col xs={12} md={4} mdPull={8} lg={5} lgPull={7}> 
+           <img id="postertest" className="poster" src={`https://image.tmdb.org/t/p/w342/${props.movie.poster_path}`} alt="Movie Poster" />
+         </Col>
+       </Col> 
+       </Grid> 
           :
           <div>
             <ReactLoading className="loadingCircle" type="spinningBubbles" color="#4444" height='100' width='100' />
@@ -29,3 +38,5 @@ const MovieShow = (props) => {
 }
 
 export default MovieShow;
+
+
