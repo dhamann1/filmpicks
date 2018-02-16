d9a950aa8f71be9934fad3d39c8b431cb5fa1cbc
Dean Hamann <hamann.dean@gmail.com>
2018-02-15 21:04:34 -0800

    Edit LoginForm into functional stateless component

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,57 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import userService from '../../utils/userService';
 import './LoginForm.css';
 
-class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      pw: ''
-    }
-  }
 
-  handleChange = (field, e) => {
-    this.setState({
-      [field]: e.target.value
-    });
-  }
+const LoginForm = (props) => {
+  return (
+    <div>
+      <header className="header-footer">Log In</header>
+      <form className="form-horizontal" onSubmit={props.handleSubmit} >
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input type="email" className="form-control" placeholder="Email" onChange={(e) => props.handleChange('email', e)} />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input type="password" className="form-control" placeholder="Password" onChange={(e) => props.handleChange('pw', e)} />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12 text-center">
+            <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;<Link to='/'>Cancel </Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    userService.login(this.state)
-      .then(() => {
-        this.props.handleLogin();
-        this.props.history.push('/');
-      })
-      .catch(err => alert('Invalid Credentials!'));
-  }
-
-  render() {
-    return (
-      <div>
-        <header className="header-footer">Log In</header>
-        <form className="form-horizontal" onSubmit={this.handleSubmit} >
-          <div className="form-group">
-            <div className="col-sm-12">
-              <input type="email" className="form-control" placeholder="Email" value={this.state.email} onChange={(e) => this.handleChange('email', e)} />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-sm-12">
-              <input type="password" className="form-control" placeholder="Password" value={this.state.pw} onChange={(e) => this.handleChange('pw', e)} />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-sm-12 text-center">
-              <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;              <Link to='/'>Cancel </Link>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
-};
 
 export default LoginForm;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import userService from '../../utils/userService';
 import './LoginPage.css';
 
-const LoginPage = (props) => {
-  return (
-    <div className='LoginPage'>
-      <LoginForm {...props} />
-    </div>
-  );
-};
 
-export default LoginPage;
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      pw: ''
+    }
+  }
+
+  handleChange = (field, e) => {
+    this.setState({
+      [field]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    userService.login(this.state)
+      .then(() => {
+        this.props.handleLogin();
+        this.props.history.push('/');
+      })
+      .catch(err => alert('Invalid Credentials!'));
+  }
+
+  render () {
+    return (
+      <div className='LoginPage'>
+        <LoginForm 
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        /> 
+      </div> 
+    )
+  }
+}
+
+  export default LoginPage;
